828648012d0ff057092a25d711f7af1802207773
Hammad Abbasi <143787522+HammadAbbasiiii@users.noreply.github.com>
2023-09-10 13:45:16 -0700

    Update app.py

--- ml_web_app-project/app.py
+++ ml_web_app-project/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello_world():
+    return render_template('hello.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
 
 
